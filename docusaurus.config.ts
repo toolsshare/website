dc85c9fd005bf5a2718998386a37aef6b491ac60
v <v@none.com>
2024-10-01 07:34:20 +0800

    禁用 github page，改用 pages.dev

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,10 +7,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://toolsshare.github.io',
+  url: 'https://tools-share.pages.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/website',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
